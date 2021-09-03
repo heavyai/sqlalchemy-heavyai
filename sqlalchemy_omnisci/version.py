f0465a6974f451a201532990f076cbf8ee4a891a
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2021-09-03 16:11:15 -0400

    Added release initial release workflow

--- sqlalchemy_omnisci/version.py
+++ sqlalchemy_omnisci/version.py
@@ -5,4 +5,12 @@ Note:
     Update this for the versions
     Don't change the forth version number from None
 """
-VERSION = (0, 0, 1, None)
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("sqlalchemy_omnisci")
+except PackageNotFoundError:
+    # package is not installed
+    __version__ = "0.0.0"
+
+VERSION = tuple(__version__.split(".")) + (None,)
