1114131e31bb3ef3947a9cf1e5af5b7372db65e8
Guilherme Leobas <guilhermeleobas@gmail.com>
2023-05-04 15:57:11 -0300

    fix warning

--- tests/test_compiler.py
+++ tests/test_compiler.py
@@ -85,7 +85,7 @@ class TestCompilerSelect(_TestCompilerHeavyai):
         for t in test_cases:
             col = column("colname").label(t["label"])
             sel_from_tbl = (
-                select([col]).group_by(col).select_from(table("abc"))
+                select(col).group_by(col).select_from(table("abc"))
             )
             compiled_result = sel_from_tbl.compile()
             assert str(compiled_result) == t["output"]
