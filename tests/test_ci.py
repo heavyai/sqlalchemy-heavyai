7858b2e92159d54cc79d25b44bc4165d8444d879
Guilherme Leobas <guilhermeleobas@gmail.com>
2023-05-04 20:00:52 -0300

    test package versions in CI

---
+++ tests/test_ci.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import pytest
+import importlib_metadata
+from packaging.version import Version
+
+
+def test_python_version():
+    varname = 'EXPECTED_PYTHON_VERSION'
+    current = Version(sys.version_info)
+    expected = os.environ.get(varname)
+    if os.environ.get('CI'):
+        assert expected is not None, (varname, current)
+    if expected is None:
+        pytest.skip(
+            f'Undefined environment variable {varname},'
+            f' cannot test python version (current={current})')
+    expected = Version(expected)
+    assert expected == current
+
+
+def test_sqlalchemy_version():
+    varname = 'EXPECTED_SQLALCHEMY_VERSION'
+    current = Version(importlib_metadata.version('sqlalchemy'))
+    expected = os.environ.get(varname)
+    if expected is None:
+        pytest.skip(
+            f'Undefined environment variable {varname},'
+            f' cannot test sqlalchemy version (current={current})')
+    expected = Version(expected)
+    assert expected == current
