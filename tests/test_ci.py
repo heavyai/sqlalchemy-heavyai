b460d936bcac21b82ccb3574d3cd854c358d40ca
Guilherme Leobas <guilhermeleobas@gmail.com>
2023-05-04 22:38:17 -0300

    Update test_ci.py

--- tests/test_ci.py
+++ tests/test_ci.py
@@ -1,34 +1,41 @@
 import os
 import sys
 
-import importlib_metadata
 import pytest
+import sqlalchemy
 from packaging.version import Version
+from sqlalchemy.testing import fixtures
 
 
-def test_python_version():
-    varname = "EXPECTED_PYTHON_VERSION"
-    current = Version(sys.version_info)
-    expected = os.environ.get(varname)
-    if os.environ.get("CI"):
-        assert expected is not None, (varname, current)
-    if expected is None:
-        pytest.skip(
-            f"Undefined environment variable {varname},"
-            f" cannot test python version (current={current})"
-        )
-    expected = Version(expected)
-    assert expected == current
+class TestCI(fixtures.TestBase):
 
+    def compare_versions(self, expected, current):
+        expected = tuple(map(int, str(expected).split('.')))
+        current = tuple(map(int, str(current).split('.')))[:len(expected)]
+        assert expected == current
 
-def test_sqlalchemy_version():
-    varname = "EXPECTED_SQLALCHEMY_VERSION"
-    current = Version(importlib_metadata.version("sqlalchemy"))
-    expected = os.environ.get(varname)
-    if expected is None:
-        pytest.skip(
-            f"Undefined environment variable {varname},"
-            f" cannot test sqlalchemy version (current={current})"
-        )
-    expected = Version(expected)
-    assert expected == current
+    def test_python_version(self):
+        varname = "EXPECTED_PYTHON_VERSION"
+        current = Version('.'.join(map(str, tuple(sys.version_info)[:3])))
+        expected = os.environ.get(varname)
+        if os.environ.get("CI"):
+            assert expected is not None, (varname, current)
+        if expected is None:
+            pytest.skip(
+                f"Undefined environment variable {varname},"
+                f" cannot test python version (current={current})"
+            )
+        expected = Version(expected)
+        self.compare_versions(expected, current)
+
+    def test_sqlalchemy_version(self):
+        varname = "EXPECTED_SQLALCHEMY_VERSION"
+        current = Version(sqlalchemy.__version__)
+        expected = os.environ.get(varname)
+        if expected is None:
+            pytest.skip(
+                f"Undefined environment variable {varname},"
+                f" cannot test sqlalchemy version (current={current})"
+            )
+        expected = Version(expected)
+        assert expected == current
